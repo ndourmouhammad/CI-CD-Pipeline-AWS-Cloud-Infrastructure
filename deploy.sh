e6378984337f723bf84cd7095a7d7b27cb59e2d1
Mouhammad Ndour <ndourmouhammad15@gmail.com>
2026-03-11 06:28:55 +0000

    feat: ajout du Dockerfile pour le portfolio

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "🚀 Lancement du déploiement automatisé..."
-echo "<h1>Portfolio de Mouhammad - Version 2.0 !</h1><p>Déployé via un script Bash.</p>" > index.html
+echo "🚀 Lancement du déploiement..."
 sudo cp index.html /var/www/html/index.html
 echo "✅ Déploiement terminé avec succès !"
